4543c7c6f7345ddce698c3505b63e1437ddf1577
Areum Yang <s2018s10@e-mirim.hs.kr>
2020-03-03 23:55:24 +0900

    ◾ pc버전 스케치북 완성

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+const againBtn = document.getElementById("jsAgain");
 
-canvas.width = canvas.offsetWidth;
-canvas.height = canvas.offsetHeight;
+const INITIAL_COLOR = "#2c2c2c";
+const WIDTH = canvas.offsetWidth;
+const HEIGHT = canvas.offsetHeight;
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,WIDTH,HEIGHT);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 3.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -34,6 +47,45 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling == true){
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick() {
+    if(!filling){
+        ctx.fillRect(0,0,WIDTH,HEIGHT);
+    }
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "My draw🎨";
+    link.click();
+}
+
+function handleAgainClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,WIDTH,HEIGHT);
 }
 
 if(canvas){
@@ -41,8 +93,26 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors, color => {
     color.addEventListener("click", handleColorClick);
 });
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
+}
+
+if(againBtn){
+    againBtn.addEventListener("click", handleAgainClick);
+}
